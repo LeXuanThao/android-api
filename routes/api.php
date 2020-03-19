4db8d7a22d10a7221da3c97dfc4ca3ed08bd96ca
quocanh99 <permertesacker99@gmail.com>
2020-03-19 23:26:31 +0700

    update api routes

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::middleware("hacked")->get('/test', 'CPUController@getCPUs');
+Route::get('/get/cpus', 'CPUController@getCPUs');
+Route::post('/user/login', 'LoginController@Login');
+Route::post('/user/create','LoginController@CreateUser');
